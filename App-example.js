8352fd2daf381d356ace1fffb355645559ed7dc0
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-09-26 19:10:17 +0100

    init

--- App-example.js
+++ App-example.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('App logger');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
